5467c8bfd8089e8599317e55d9d1ab4ca7e1076a
kananazizli <kananazizli.ao@gmail.com>
2022-07-21 07:02:35 -0400

    Added 1st Codewars Challenge (C++) "Basic Variable Assignment"

---
+++ 8kyu/basic_variable_assignment/basic_variable_assignment.cpp
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+
+string firstPart = "code";
+string secondPart = "wa.rs";
+string name = firstPart + secondPart;
+
+
+/* This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+#include <string>
+
+std::string a == "code";
+std::string b == "wa.rs";
+std::string name == a + b;
+*/
